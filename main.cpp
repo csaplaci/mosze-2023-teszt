07416a6366d7d76aefff3e444568964f8178e31b
László Csaplár <csapplaci@gmail.com>
2023-09-24 22:33:09 +0200

    docs(main.cpp): code review with in-line comments

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];                    // missing deallocation ("delete") - this may result in memory leak
+    std::cout << '1-100 ertekek duplazasa'          // missing semicolon at the end of command + single quotes instead of double (= string, not char) + endl would be nice
+    for (int i = 0;)                                // rest of the for loop is missing (only init, no condition or increment)
     {
-        b[i] = i * 2;
+        b[i] = i * 2;                               // probably meant "b[i] = (i+1) * 2" as this would do 0-99 instead of 1-100
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)                         // checks "i" as bool, probably meant "i<100" instead (as the condition)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"                       // missing semicolon + missing actual values (probably meant "Ertek:" once and only actual values in loop)
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)            // colon instead of the 2nd semicolon
     {
-        atlag += b[i]
+        atlag += b[i]                               // missing semicolon
     }
-    atlag /= N_ELEMENTS;
+    atlag /= N_ELEMENTS;                            // ! atlag is int -> remainder will be lost (if unintentional, try float or double) !
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
